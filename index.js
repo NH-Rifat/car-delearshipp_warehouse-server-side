e88f55c9e117e00feea36bf6584c2b1a45921a96
NH-Rifat <rifatr729@gmail.com>
2022-05-06 16:10:58 +0600

    car product api working successfully

--- index.js
+++ index.js
@@ -17,13 +17,6 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.055ti.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   serverApi: ServerApiVersion.v1,
-// });
 async function run() {
   try {
     await client.connect();
@@ -35,13 +28,13 @@ async function run() {
       res.send(services);
     });
   } finally {
-    // await client.close();
+    
   }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('car dealer sever');
+  res.send('car dealer sever connected');
 });
 
 app.listen(port, () => {
